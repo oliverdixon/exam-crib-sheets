dac6db218bbcada3ac6985b2521db1893fa8528e
Oliver Dixon <od641@york.ac.uk>
2023-04-25 03:08:43 +0100

    Added rasters for accessibility
    Rasters are not tracked in version control. See the website
    https://www-users.york.ac.uk/~od641/exam-crib-sheets/ for document
    exports.

--- publish.sh
+++ publish.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 # OWD 2023
 
+# This script requires a moderately recent version of GhostScript (10+, for the
+# new C-based PDF parser) and a copy of ImageMagick with the PDF and PNG codecs
+# installed and enabled.
+
 SSH_URL="od641@maxwell.york.ac.uk:~/web/exam-crib-sheets/"
 GIT_ROOT="$(git rev-parse --show-toplevel)"
 INDEX="$GIT_ROOT/publishing-index"
-publish_list=()
+remote_list=()
 
 # compress_pdf: Given a single PDF file, use GhostScript to perform an in-place
 # compression, and report the compression ratio to stdout. The GS return code is
@@ -12,7 +16,6 @@ publish_list=()
 
 compress_pdf () {
         local TMP_NAME="$(dirname "$1")/tmp-$(date '+%s')-$RANDOM.pdf"
-        local PRINT_NAME="$(realpath --relative-to=./ "$1")"
 
         gs                               \
                 -sDEVICE=pdfwrite        \
@@ -21,12 +24,13 @@ compress_pdf () {
                 -dQUIET                  \
                 -dBATCH                  \
                 -dPrinted=false          \
-                -sOutputFile="$TMP_NAME" "$1"
+                -sOutputFile="$TMP_NAME" \
+                "$1"
 
         local ret=$?
 
         if [[ $ret -eq 0 ]]; then
-                echo -e "$PRINT_NAME\t$(du -h "$1" | cut -f1) ->" \
+                echo -e "Compressed $1 from $(du -h "$1" | cut -f1) to" \
                         "$(du -h --apparent-size "$TMP_NAME" | cut -f1)"
                 mv "$TMP_NAME" "$1"
         fi
@@ -34,24 +38,61 @@ compress_pdf () {
         return $ret
 }
 
-# We first compress each file listed in the index that does not have a
-# GhostScript invocation marker. GREP is well-defined to return '1' when the
-# file was successfully opened, but no matching lines were found. If GS is
-# required, but did not execute successfully, then its return code is forwarded.
+# update_raster: If the source PDF is newer than its corresponding raster, use
+# ImageMagick to regenerate the PNG, and report the change. If a raster is
+# out-of-date, and ImageMagick fails, its non-zero error code is forwarded.
+# Otherwise, zero is returned.
 
-echo "Processing Uncompressed Files..."
+update_raster () {
+        local RASTER_NAME="${1%.*}_Raster"
+        local ret=0
+
+        # We only check the last-modified date of the first image page, since
+        # the entire cluster would've been generated together.
+
+        if [[ ${RASTER_NAME}-0.png -ot $1 ]]; then
+                convert                   \
+                        -density 150      \
+                        -background white \
+                        -alpha remove     \
+                        "$1"              \
+                        "${RASTER_NAME}.png"
+
+                ret=$?
+                [[ $ret -eq 0 ]] && echo "Updated rasters for $1"
+        fi
+
+        return $ret
+}
+
 while read file; do
-        file="$GIT_ROOT/${file}"
+        file="$(realpath --relative-to=./ "$GIT_ROOT/$file")"
 
         if [[ -f "$file" ]]; then
-                publish_list+=("$file")
-                grep -qs "%%Invocation: gs" "$file"
+                # We first compress each file listed in the index that does not
+                # have a GhostScript invocation marker. GREP is well-defined to
+                # return '1' when the file was successfully opened, but no
+                # matching lines were found. If GS is required, but did not
+                # execute successfully, then its return code is forwarded.
 
+                grep -qs "%%Invocation: gs" "$file"
                 if [ $? -eq 1 ]; then
                         compress_pdf "$file"
                         ret=$?
                         [ $ret -ne 0 ] && exit $ret
                 fi
+
+                # Re-generate rasters for the given file, where necessary.
+
+                update_raster "$file"
+                ret=$?
+                [ $ret -ne 0 ] && exit $ret
+
+                # Update the remote publication list, assuming that two raster
+                # pages were generated for each file.
+
+                remote_list+=("$file" "${file%.*}_Raster-0.png" \
+                        "${file%.*}_Raster-1.png")
         fi
 done < "$INDEX"
 
@@ -59,13 +100,13 @@ done < "$INDEX"
 # remote with rsync, where the local copy is newer. The rsync return code is
 # forwarded.
 
-if [ ${#publish_list[@]} -ne 0 ]; then
-        echo -e "\nPublishing to the Remote... (Requires York VPN access)"
-        rsync -vtu --ignore-missing-args -e 'ssh -q' "${publish_list[@]}" \
+if [ ${#remote_list[@]} -ne 0 ]; then
+        echo -e "Publishing to the Remote... (Requires York VPN access)\n"
+        rsync -vtu --ignore-missing-args -e 'ssh -q' "${remote_list[@]}" \
                 "$SSH_URL"
         ret=$?
 else
-        echo -e "\nNothing to do!"
+        echo "Nothing to upload!"
         ret=0
 fi
 
